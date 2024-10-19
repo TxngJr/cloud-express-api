b72618d0964c44e1703d097d8c052d627df7ba7b
Denmann N'ramzz <116389026+TxngJr@users.noreply.github.com>
2024-10-19 23:18:48 +0700

    Update file.route.ts

--- src/routes/file.route.ts
+++ src/routes/file.route.ts
@@ -4,7 +4,7 @@ import multer from "multer";
 const path = require("path");
 
 const storage = multer.diskStorage({
-    destination: "./uploads/",
+    destination: "~/uploads/",
     filename: function (req, file, cb) {
       const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
       const fileExtension = path.extname(file.originalname);
